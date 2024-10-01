d9e51022ee1d4b046397e55f9ef21da49adb4e35
cahit <cahit.yildirim@bilkent.edu.tr>
2024-10-01 07:46:53 +0300

    add empirical model tests

---
+++ test/models/test_empirical_mean_var.py
@@ -0,0 +1,124 @@
+from unittest import mock, TestCase
+
+import numpy as np
+
+from vectoptal.models import EmpiricalMeanVarModel
+from vectoptal.order import Order
+from vectoptal.datasets import Dataset
+
+
+class TestEmpiricalMeanVarModel(TestCase):
+    """Test the EmpiricalMeanVarModel class."""
+
+    def setUp(self):
+        # A basic setup for the model.
+        self.input_dim = 2
+        self.output_dim = 2
+        self.noise_var = 0.1
+        self.design_count = 3
+        self.track_means = True
+        self.track_variances = True
+        self.model = EmpiricalMeanVarModel(
+            input_dim=self.input_dim,
+            output_dim=self.output_dim,
+            noise_var=self.noise_var,
+            design_count=self.design_count,
+            track_means=self.track_means,
+            track_variances=self.track_variances,
+        )
+
+    def test_add_sample(self):
+        """Test the add_sample method."""
+        with self.assertRaises(AssertionError):
+            indices = [0]
+            Y_t = np.array([[1, 2], [3, 4]])
+            self.model.add_sample(indices, Y_t)
+        
+        with self.assertRaises(AssertionError):
+            indices = [6]
+            Y_t = np.array([[1, 2]])
+            self.model.add_sample(indices, Y_t)
+    
+        indices = [0, 0, 1]
+        Y_t = np.array([[1, 2], [3, 4], [5, 6]])
+        self.model.add_sample(indices, Y_t)
+        sample_counts = [2, 1, 0]
+        for idx, count in enumerate(sample_counts):
+            with self.subTest(idx=idx):
+                self.assertEqual(self.model.design_samples[idx].shape[0], count)
+    
+    def test_clear_data(self):
+        """Test the clear_data method."""
+        self.model.clear_data()
+        for i in range(self.design_count):
+            with self.subTest(i=i):
+                self.assertEqual(self.model.design_samples[i].shape, (0, self.output_dim))
+    
+    def test_update(self):
+        """Test the update method."""
+        self.model.add_sample(
+            [0, 0, 1],
+            np.array([[1, 2], [3, 4], [5, 6]])
+        )
+
+        self.model.track_means = False
+        self.model.track_variances = False
+        self.model.update()
+        self.assertIsNone(self.model.means)
+        self.assertIsNone(self.model.variances)
+        
+        self.model.track_means = True
+        self.model.track_variances = True
+        self.model.update()
+        means_tracked = np.array([[2, 3], [5, 6], [0, 0]])
+        variances_tracked = np.array([
+            [[1, 0], [0, 1]],
+            [[self.noise_var, 0], [0, self.noise_var]],
+            [[self.noise_var, 0], [0, self.noise_var]]
+        ])
+        np.testing.assert_allclose(self.model.means, means_tracked)
+        np.testing.assert_allclose(self.model.variances, variances_tracked)
+    
+    def test_predict(self):
+        """Test the predict method."""
+        self.model.add_sample(
+            [0, 0, 1],
+            np.array([[1, 2], [3, 4], [5, 6]])
+        )
+        self.model.update()
+
+        # NOTE: Actual coordinates of test points are not used, only the indices are used.
+
+        with self.assertRaises(AssertionError):
+            self.model.predict(np.array([[-1, -1]]))
+
+        X = np.array([
+            [-1, -1, 0],
+            [-1, -1, 1]
+        ])
+
+        # Test if the predict method returns the correct output when track_means
+        # and track_variances are disabled.
+        self.model.track_variances = False
+        self.model.track_means = False
+        means = np.array([[0, 0], [0, 0]])
+        variances = np.array([
+            [[1, 0], [0, 1]],
+            [[1, 0], [0, 1]]
+        ])
+        out_mean, out_variances = self.model.predict(X)
+        np.testing.assert_allclose(out_mean, means)
+        np.testing.assert_allclose(out_variances, variances)
+        
+        # Test if the predict method returns the correct output when track_means
+        # and track_variances are enabled.
+        self.model.track_variances = True
+        self.model.track_means = True
+        means = np.array([[2, 3], [5, 6]])
+        variances = np.array([
+            [[1, 0], [0, 1]],
+            [[self.noise_var, 0], [0, self.noise_var]]
+        ])
+        out_mean, out_variances = self.model.predict(X)
+        np.testing.assert_allclose(out_mean, means)
+        np.testing.assert_allclose(out_variances, variances)
--- vectoptal/models/empirical_mean_var.py
+++ vectoptal/models/empirical_mean_var.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 import numpy as np
 
 from vectoptal.models import Model
@@ -21,34 +23,70 @@ class EmpiricalMeanVarModel(Model):
         # Data containers.
         self.clear_data()
 
-    def add_sample(self, indices, Y_t):
-        # Last column of X_t are sample space indices.
+    def add_sample(self, indices: Iterable[int], Y_t: np.ndarray):
+        """
+        Add new samples for specified design indices.
+
+        :param indices: Represents the indices of designs to which the samples belong.
+        :type indices: Iterable[int]
+        :param Y_t: A N-by-output_dim array containing the new samples to be added.
+        :type Y_t: np.ndarray
+        """
+        assert len(indices) == len(Y_t), "Number of samples is ambiguous."
+        assert max(indices) < self.design_count, "Design index out of bounds."
+
         for idx, y in zip(indices, Y_t):
             self.design_samples[idx] = np.concatenate(
                 [self.design_samples[idx], y.reshape(-1, self.output_dim)], axis=0
             )
 
     def clear_data(self):
+        """
+        This method generates/clears the sample containers.
+        """
         self.design_samples = [np.empty((0, self.output_dim)) for _ in range(self.design_count)]
 
     def update(self):
+        """
+        This method calculates and updates the means and variances of the design samples based on the current data.
+        If `track_means` is enabled, it updates the `means` attribute with the mean of each design sample.
+        If `track_variances` is enabled, it updates the `variances` attribute with the variance of each design sample.
+        """
         if self.track_means:
             self.means = np.array([
                 np.mean(design, axis=0)
                 if len(design) > 0 else np.zeros(self.output_dim)
                 for design in self.design_samples
             ])
+        else:
+            self.means = None
+
         if self.track_variances:
             self.variances = np.array([
                 np.diag(np.var(design, axis=0))
                 if len(design) > 1 else np.eye(self.output_dim) * self.noise_var
                 for design in self.design_samples
             ])
+        else:
+            self.variances = None
 
     def train(self):
         pass
 
-    def predict(self, test_X) -> tuple[np.ndarray, np.ndarray]:
+    def predict(self, test_X: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """
+        This method takes test inputs and returns the predicted means and variances based on the tracked data.
+        If `track_means` is enabled, it returns the corresponding means for the test inputs.
+        If `track_variances` is enabled, it returns the corresponding variances for the test inputs.
+
+        :param test_X: The test inputs for which predictions are to be made. The last column of `test_X` should contain indices.
+        :type test_X: np.ndarray
+        :return: A tuple containing two numpy arrays: the predicted means and variances.
+        :rtype: tuple[np.ndarray, np.ndarray]
+        """
+        assert test_X.shape[1] == self.output_dim + 1, \
+            "Test data needs to have an additional column for indices."
+
         indices = test_X[..., -1].astype(int)
         if self.track_means:
             means = self.means[indices]
