6c6d33ae2701a3fbb116775ff7e9da6bd57f3c50
cahit <cahit.yildirim@bilkent.edu.tr>
2024-11-04 17:34:40 +0300

    docs conf mock import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..", "..")))
+autodoc_mock_imports = ["numpy"]
 
 
 # -- Project information -----------------------------------------------------
